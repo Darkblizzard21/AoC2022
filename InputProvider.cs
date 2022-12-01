9fc9af503351d5e7bf8a165c7efa04e433d14da6
Pfeifer, Pirmin <Pirmin.Pfeifer@amd.com>
2022-12-01 11:17:13 +0100

    Day 01

---
+++ InputProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Collections.Specialized;
+
+namespace AoC2022
+{
+    public class InputProvider
+    {
+        string path;
+
+        public InputProvider(string day)
+        {
+            path = ConfigurationManager.AppSettings.Get("InputFolder") + "\\" + day + "\\";
+        }
+
+        public string GetInput()
+        {
+            return File.ReadAllText(path + "input.txt");
+        }
+    }
+}
