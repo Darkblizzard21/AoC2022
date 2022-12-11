a9ff90105f2e224fe2721197afa2e6ca3f9ecd57
Pirmin Pfeifer <pirmin.pfeifer@stud-mail.uni-wuerzburg.de>
2022-12-11 07:38:40 +0100

    Array Extensions

---
+++ util/ArrayExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2022.util
+{
+    public static class ArrayExtensions
+    {
+        public static string ToFormmatedString(this char[][] array) => array.Select(l => l.Aggregate("", (p, c) => p + c)).Aggregate((p, c) => p + "\n" + c);
+    }
+}
